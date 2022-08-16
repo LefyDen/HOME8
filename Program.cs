7dfd822e5f2bc81b664752a1f1d96c3a5d0fb79c
LefyDen <denisyuk.denis.s@gmail.com>
2022-08-16 13:08:07 +0300

    inital version

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using System.Text.RegularExpressions;
+namespace HOME8_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] phoneBook = { "Clara Klane 380670356345", "li Lion 380340356745", "Clara Frog 380478356745" };
+            string ss;
+
+            ss = Console.ReadLine();
+            Regex regex = new Regex(ss);
+            int hMatcher;
+
+            for (int i = 0; i < phoneBook.Length; i++)
+            {
+                string s = phoneBook[i];
+
+
+
+                MatchCollection matches = regex.Matches(s);
+                hMatcher = matches.Count;
+                if (matches.Count != 0)
+                {
+
+                    Console.WriteLine(phoneBook[i]);
+
+                }
+                if (hMatcher < 1 && i == phoneBook.Length)
+                {
+                    Console.WriteLine("Нікого не знайдено");
+                }
+            }
+        }
+    }
+}

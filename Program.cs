17fb07daa9becf91cc121644aae0e36b5fd62ae7
LefyDen <denisyuk.denis.s@gmail.com>
2022-08-19 13:55:06 +0300

    help

--- Program.cs
+++ Program.cs
@@ -1,17 +1,19 @@
 ﻿
 using System;
 using System.Text.RegularExpressions;
+using System.IO;
 namespace HOME8_2_
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string[] phoneBook = { "Clara Klane 380670356345", "li Lion 380340356745", "Clara Frog 380478356745" };
+            string[] phoneBook = { "Clara Klane +380670356345", "li Lion +380340356745", "Clara Frog +380478356745" };
             string ss;
-
             ss = Console.ReadLine();
-            Regex regex = new Regex(ss);
+            try
+            {
+               Regex regex = new Regex(ss);
             int hMatcher;
 
             for (int i = 0; i < phoneBook.Length; i++)
@@ -33,6 +35,15 @@ namespace HOME8_2_
                     Console.WriteLine("Нікого не знайдено");
                 }
             }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Введiть коректнi даннi, без '+'");
+                Console.WriteLine(ex.Message);
+            }
+
+
         }
     }
 }
